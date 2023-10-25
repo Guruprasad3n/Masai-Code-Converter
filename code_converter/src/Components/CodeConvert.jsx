bce3aa0516187b1f7aaba1a8c71ebf0e0f1bcc1c
Guruprasad3n <mr.guruprasadb@gmail.com>
2023-10-26 00:58:56 +0530

    Initial

--- code_converter/src/Components/CodeConvert.jsx
+++ code_converter/src/Components/CodeConvert.jsx
@@ -1,14 +1,25 @@
+import axios from "axios";
 import React, { useState } from "react";
 
 function CodeConvert() {
   const [inputCode, setInputCode] = useState("");
   const [convertedCode, setConvertedCode] = useState("");
+  const [selectLang, setSelectlang] = useState("");
   const [loading, setLoading] = useState(false);
 
+//   , {
+//     code: inputCode,
+//     lang: selectlang,
+//   }
+//   http://localhost:8000/convert?code${inputCode}&lang=${selectlang}
   const handleConvert = () => {
+    
     setLoading(true);
     axios
-      .post("http://localhost:8000/convert", { code: inputCode })
+      .post(` http://localhost:8000/convert`,{
+        code: inputCode,
+        lang:selectLang
+      })
       .then((response) => {
         setConvertedCode(response.data.convertedCode);
       })
@@ -25,13 +36,18 @@ function CodeConvert() {
       <div className="code_converter_main">
         <h1>Code Converter</h1>
         <div className="heading_child">
-          <select name="" id="">
+          <select
+            name="selectlang"
+            id="selectlang"
+            value={selectLang}
+            onChange={(e) =>{ setSelectlang(e.target.value), console.log("Cliccked")}}
+          >
             <option value="" hidden>
               Select language
             </option>
-            <option value="">Python</option>
-            <option value="">Java</option>
-            <option value="">JavaScript</option>
+            <option value="python">Python</option>
+            <option value="java">Java</option>
+            <option value="javascript">JavaScript</option>
           </select>
         </div>
         <div className="code_converter_child">
@@ -60,7 +76,88 @@ function CodeConvert() {
     </>
   );
 }
+
 export default CodeConvert;
+
+
+
+
+
+
+
+
+
+// import axios from "axios";
+// import React, { useState } from "react";
+
+// function CodeConvert() {
+//   const [inputCode, setInputCode] = useState("");
+//   const [convertedCode, setConvertedCode] = useState("");
+//   const [selectlang, setSelectlang] = useState("");
+//   const [loading, setLoading] = useState(false);
+
+// //   http://localhost:8000/shayari?type=${selectShayari}&keyword=${keyword}
+// // http://localhost:8000/convert?code${inputCode}&lang=${selectlang}
+
+//   const handleConvert = () => {
+//     setLoading(true);
+//     axios
+//       .post(`http://localhost:8000/convert`, {
+//         code: inputCode,
+//         lang: selectlang,
+//       })
+//       .then((response) => {
+//         setConvertedCode(response.data.convertedCode);
+//       })
+//       .catch((error) => {
+//         console.error("Error converting code:", error);
+//       })
+//       .finally(() => {
+//         setLoading(false);
+//       });
+//   };
+
+//   return (
+//     <>
+//       <div className="code_converter_main">
+//         <h1>Code Converter</h1>
+//         <div className="heading_child">
+//           <select name="" id="" value={selectlang} onChange={(e) => setSelectlang(e.target.value)}>
+//             <option value="" hidden>
+//               Select language
+//             </option>
+//             <option value="python">Python</option>
+//             <option value="java">Java</option>
+//             <option value="javascript">JavaScript</option>
+//           </select>
+//         </div>
+//         <div className="code_converter_child">
+//           <div className="code_converter_child_left">
+//             <div>
+//               <textarea
+//                 placeholder="Enter code here"
+//                 rows="30"
+//                 cols="50"
+//                 value={inputCode}
+//                 onChange={(e) => setInputCode(e.target.value)}
+//               ></textarea>
+//             </div>
+//             <div>
+//               <button onClick={handleConvert} disabled={loading}>
+//                 {loading ? "Converting..." : "Convert Code"}
+//               </button>
+//             </div>
+//           </div>
+//           <div className="code_converter_child_right">
+//             <h2>Converted Code</h2>
+//             <pre>{convertedCode}</pre>
+//           </div>
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
+// export default CodeConvert;
 
 {
   /* <div>
--- index.js
+++ index.js
@@ -5,47 +5,44 @@ const dotenv = require("dotenv").config();
 
 const app = express();
 
-const API_KEY = process.env.OPENAI_API_KEY
+const API_KEY = process.env.OPENAI_API_KEY;
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const PORT = process.env.PORT || 5000 ;
+const PORT = process.env.PORT || 5000;
 
 app.get("/", async (req, res) => {
   res.status(200).send("Welcome to Home Page..!");
 });
 
 // Chat GPT Post Request
-app.post('/convert', async (req, res) => {
-    const { code } = req.body;
-  
+app.post("/convert", async (req, res) => {
+  const { code, lang } = req.body;
 
-    try {
-      const response = await axios.post(
-        'https://api.openai.com/v1/engines/text-davinci-003/completions',
-        {
-          prompt: `Convert This code to Java Programming language: ${code}`,
-          max_tokens: 100,
-          temperature:1,
+  try {
+    const response = await axios.post(
+      "https://api.openai.com/v1/engines/text-davinci-003/completions",
+      {
+        prompt: `Convert This ${code} to ${lang} Programming language`,
+        max_tokens: 100,
+        temperature: 1,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${API_KEY}`,
         },
-        {
-          headers: {
-            'Authorization': `Bearer ${API_KEY}`,
-          },
-        }
-      );
-  
-      const convertedCode = response.data.choices[0].text;
-      res.json({ convertedCode });
-    } catch (error) {
-      console.error('Error converting code:', error);
-      res.status(500).json({ error: 'Error converting code' });
-    }
-  });
+      }
+    );
 
-
+    const convertedCode = response.data.choices[0].text;
+    res.json({ convertedCode });
+  } catch (error) {
+    console.error("Error converting code:", error);
+    res.status(500).json({ error: "Error converting code" });
+  }
+});
 
 app.listen(PORT, (req, res) => {
   console.log(`Server started in http://localhost:${PORT}`);
